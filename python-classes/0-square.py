9e30c990ae7870c9921adbd70ffc6ac3dc5c7543
SabyJeany <sabyjeany@gmail.com>
2026-01-29 16:45:23 +0100

    Write an empty square that definies a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""0-square module
+"""
+0-square module
 This module defines an empty class Square.
 """
 
 class Square:
-    """ represents an empty square class """
+    """ 
+    represents an empty square class
+    """
     pass
