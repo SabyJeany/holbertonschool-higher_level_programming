4617d7d0bb00f7750f48c8a0932a0ac1d2adab37
SabyJeany <sabyjeany@gmail.com>
2026-02-06 20:53:37 +0100

    Write class BaseGeometry based on 5_base

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    A class BaseGeometry with a public instance method area
+    """
+    def area(self):
+        """
+        Raises an Exception with a message.
+        """
+        raise Exception("area() is not implemented")
